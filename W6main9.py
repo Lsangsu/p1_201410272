65de36b44e572eb669931274029756fac558a953
Lsangsu <sangsu218@naver.com>
2016-04-06 15:08:18 +0900

    Update W6main9.py

--- W6main9.py
+++ W6main9.py
@@ -1,9 +1,9 @@
-sum=0
-for i in range(0,1001):
-    if i%3==0:
-        sum+=i
-    elif i%5==0:
-        sum+=i
-    elif i%15==0:
-        sum-=i
-print sum
+def sumOfMultipleOf3_5(begin,end):
+    sum=0
+    for i in range(begin,end):
+        if i%3==0 or i%5==0:
+            sum+=i
+        elif i%15==0:
+            sum-=i          
+    return sum
+sumOfMultipleOf3_5(1,1000)
