3f8c8a49533fd811dfd361aab9814417c54f795e
Lsangsu <sangsu218@naver.com>
2016-04-07 16:38:17 +0900

    Added files via upload

---
+++ W6UpDown.py
@@ -0,0 +1,18 @@
+﻿def UpDown(FindNum,ThinkNum):
+        if FindNum>ThinkNum:
+            print "UP!!"
+        elif  FindNum<ThinkNum:
+            print "DOWN!!"
+        elif FindNum==ThinkNum:
+            print "Correct"
+
+def lab6():
+    FindNum=raw_input("Set Number!!")
+    ThinkNum=raw_input("FInd Number~ ")
+    UpDown(FindNum,ThinkNum)
+
+def main():
+    lab6()
+
+if __name__=="__main__":
+    main()
