51de338fae20e02b51a99222bdda1f2607bd82fe
Lsangsu <sangsu218@naver.com>
2016-04-18 13:51:46 +0900

    Added files via upload

---
+++ W7main.py
@@ -0,0 +1,24 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def drawSquareAtSace(size,pos):
+    tracks=list()
+    t1.penup()
+    t1.setpos(pos,0)
+    t1.pendown()
+    for i in range(0,4):
+        t1.fd(100)
+        t1.right(90)
+        tracks.append(t1.pos())
+    print tracks
+
+def lab7():
+    drawSquareAtSace(100,100)
+
+def main():
+    lab7()
+    
+
+if __name__=="__main__":
+	main()
