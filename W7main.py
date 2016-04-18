36c5690207b84368da260f83d7be0c2c50ca4321
Lsangsu <sangsu218@naver.com>
2016-04-18 20:23:40 +0900

    Update W7main.py

--- W7main.py
+++ W7main.py
@@ -1,20 +1,41 @@
-﻿import turtle
-wn=turtle.Screen()
-t1=turtle.Turtle()
-
 def drawSquareAtSave(size,pos):
+    import turtle 
+    wn = turtle.Screen() 
+    t1= turtle.Turtle() 
     tracks=list()
     t1.penup()
     t1.setpos(pos)
     t1.pendown()
     for i in range(0,4):
-        t1.fd(100)
+        t1.fd(size)
         t1.right(90)
         tracks.append(t1.pos())
     print tracks
+    
+    
+def drawSquareAtTrack(size,pos):
+    import turtle 
+    wn = turtle.Screen() 
+    t1= turtle.Turtle() 
+    tracks=list()
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    for i in range(0,4):
+        t1.fd(size)
+        t1.right(90)
+        tracks.append(t1.pos())
+    t1.home()
+    t1.clear()
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    for i in range(0,4):
+        t1.goto(tracks[i])
 
 def lab7():
     drawSquareAtSave(100,(100,100))
+    drawSquareAtTrack(50,(-50,-50))
 
 def main():
     lab7()
