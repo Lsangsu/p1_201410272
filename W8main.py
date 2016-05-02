babc9e55ea635d968ff462f3d225a1f3d0cd6568
Lsangsu <sangsu218@naver.com>
2016-05-02 13:09:08 +0900

    Added files via upload

---
+++ W8main.py
@@ -0,0 +1,21 @@
+﻿def charCount():
+    word=raw_input('Enter the word~')
+    d=dict()
+    for c in word:
+        if c not in d:
+            d[c]=1
+        else:
+            d[c]+=1
+    print word
+    print d
+
+
+def lab8():
+    charCount()
+
+def main():
+    lab8()
+    
+
+if __name__=="__main__":
+    main()
