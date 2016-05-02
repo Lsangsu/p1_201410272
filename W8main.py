d19b7f48ca748b0867db6f30ca57d3692ba598ad
Lsangsu <sangsu218@naver.com>
2016-05-02 13:32:24 +0900

    Update W8main.py

--- W8main.py
+++ W8main.py
@@ -9,9 +9,26 @@
     print word
     print d
 
+def countDigitsLetters():
+    word=raw_input('Enter the word~')
+    d=dict()
+    d['number']=0
+    d['text']=0
+    for c in word:
+        if c.isdigit():
+            d['number']+=1
+        else:
+            d['text']+=1
+    print d
+    import matplotlib
+    import matplotlib.pyplot as plt
+    plt.bar(range(len(d)), d.values(),align='center')
+    plt.xticks(range(len(d)),list(d.keys()))
+    plt.show()
 
 def lab8():
     charCount()
+    countDigitsLetters()
 
 def main():
     lab8()
