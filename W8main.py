19ae3e8b2a393cbfdda68a1282a8a4a973352be0
Lsangsu <sangsu218@naver.com>
2016-05-04 15:01:53 +0900

    Update W8main.py

--- W8main.py
+++ W8main.py
@@ -26,20 +26,10 @@ def countDigitsLetters():
     plt.xticks(range(len(d)),list(d.keys()))
     plt.show()
     
-def distance():
-    Locations=list()
-    Locations=[(37.576484, 126.985447),(37.574480, 126.957821),(37.571516, 126.976580),(37.570182, 126.983029),(37.563565, 126.975394)]
-    import math
-    d=list()
-    for i in range(0,len(Locations)):
-        d.append(math.sqrt((37.575883-Locations[i][0])**2 + (126.973480-Locations[i][1])**2))
-    print d
-    print min(d)
 
 def lab8():
     charCount()
     countDigitsLetters()
-    distance()
 
 def main():
     lab8()
