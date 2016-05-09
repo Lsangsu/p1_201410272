f6892078101b985ec61e5d9da2d69bd3b1738ec7
Lsangsu <sangsu218@naver.com>
2016-05-09 13:54:52 +0900

    Update W10main.py

--- W10main.py
+++ W10main.py
@@ -82,11 +82,27 @@ def ScoreAverage():
     print 'English Sum :', EnglishSum, 'English Average :', EnglishSum/E
     print 'Math Sum :', MathSum, 'Math Average :', MathSum/E
     
+def Song():
+    LetItBe=(
+    "when I find myself in times of trouble mother mary comes to me speaking words of wisdom let it be and in my hour of darkness she is standing right in front of me speaking words of wisdom let it be let it be let it be let it be let it be whisper words of wisdom let it be and when the broken-hearted people living in the world agree there will be an answer let it be for though they may be parted there is still a chance that they will see there will be an answer let it be let it be let it be let it be let it be yeah there will be an answer let it be let it be let it be let it be let it be whisper words of wisdom let it be let it be let it be ah let it be yeah let it be whisper words of wisdom let it be and when the night is cloudy there is still a light that shines on me shine on until tomorrow let it be i wake up to the sound of music mother Mary comes to me speaking words of wisdom let it be let it be let it be let it be yeah let it be oh there will be an answer let it be let it be let it be let it be yeah let it be hisper words of wisdom let it be"
+    )
+    Word=dict()
+    for i in LetItBe.split():
+        if i not in Word:
+            Word[i]=1
+        else:
+            Word[i]+=1
+    print Word
+    for i in Word:
+        if Word[i] > 10:
+            print i,':',Word[i],'Time'
+            
 def lab10():
     distance()
     popSumG()
     Milk()
     ScoreAverage()
+    Song()
 
 def main():
     lab10()
