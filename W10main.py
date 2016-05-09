b06894015c38fbe9954b41d515f9555466bf8479
Lsangsu <sangsu218@naver.com>
2016-05-09 12:19:28 +0900

    Update W10main.py

--- W10main.py
+++ W10main.py
@@ -8,8 +8,51 @@
     print d
     print min(d)
 
+def popSumG():
+    pop=[[74425, 76326],
+    [61164, 61636],
+    [109688, 115744],
+    [144796, 146776],
+    [174996, 181676],
+    [177841, 177434],
+    [204630, 205980],
+    [223373, 232245],
+    [161055, 166130],
+    [171576, 176735],
+    [279169, 293772],
+    [239450, 251066],
+    [148690, 156510],
+    [182977, 196992],
+    [237792, 242641],
+    [283869, 296762],
+    [209344, 210282],
+    [118965, 114441],
+    [186503, 186856],
+    [195734, 203014],
+    [254381, 249472],
+    [212401, 229111],
+    [271654, 295354],
+    [319197, 335045],
+    [229829, 231671]]
+    malesum=0
+    femalesum=0
+    for i in pop:
+        malesum+=i[0]
+        femalesum+=i[1]
+    print 'Sum of male: ', malesum,' Average of male: ', malesum/len(pop)
+    print 'Sum on female: ', femalesum,' Average of female: ', femalesum/len(pop)
+    popSum=list()
+    for i in pop:
+        popSum.append(i[0]+i[1])
+    print popSum
+    import matplotlib
+    import matplotlib.pyplot as plt
+    plt.bar(range(len(popSum)), popSum,align='center')
+    plt.show()
+    
 def lab10():
     distance()
+    popSumG()
 
 def main():
     lab10()
