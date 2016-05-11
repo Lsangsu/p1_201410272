98c35514c09a47ccf64bd002d6542f510bd0ac05
Lsangsu <sangsu218@naver.com>
2016-05-11 15:52:54 +0900

    Add files via upload

---
+++ W11main.py
@@ -0,0 +1,36 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def Keyup():
+	t1.forward(50)
+def Keydown():
+	t1.back(50)
+def Keyright():
+	t1.right(90)
+def Keyleft():
+	t1.left(90)
+def End():
+	wn.bye()
+
+def mousegoto(x,y):
+	t1.setpos(x,y)
+
+def addKeys():
+	wn.onkey(Keyup,"Up")
+	wn.onkey(Keydown,"Down")
+	wn.onkey(Keyright,"Right")
+	wn.onkey(Keyleft,"Left")
+	wn.onkey(End,"q")
+
+def addMouse():
+	wn.onclick(mousegoto)
+
+
+
+def lab11():
+	addMouse()
+	addKeys()
+	wn.listen()
+	turtle.mainloop()
+
+lab11()
