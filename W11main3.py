c9dc4750ea89a6844aaba4e42971653141edad0f
Lsangsu <sangsu218@naver.com>
2016-05-16 13:09:28 +0900

    Update and rename W11main/MyMazeEvent.py to W11main3.py

--- W11main3.py
+++ W11main3.py
@@ -2,27 +2,22 @@
 wn=turtle.Screen()
 t1=turtle.Turtle()
 t1.shape("turtle")
-wn.bgpic("C:\Users\P400\Desktop\myMaze.gif")
+wn.bgpic("myMaze.gif")
 def Keyup():
      t1.fd(50)
-
 
 def Keydown():
      t1.back(50)
 
-
 def Keyright():
      t1.right(90)
-
 
 def Keyleft():
      t1.left(90)
 
-
 def Mousegoto(x,y):
      t1.setpos(x,y)
-
-
+     
 def addKeys():
      wn.onkey(Keyup,"Up")
      wn.onkey(Keydown,"Down")
