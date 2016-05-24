8ebf2e4f3b655d28c6f28a64fc1fe88031c2df6e
Lsangsu <sangsu218@naver.com>
2016-05-24 12:15:52 +0900

    Add files via upload

---
+++ W12main/W12main.py
@@ -0,0 +1,23 @@
+﻿import os
+def W12main1():
+    try:
+        mydir=os.getcwd() 
+        filename='Python.txt' 
+        myfilename=os.path.join(mydir, filename) 
+        myfile = open(myfilename,'r')
+        contents=myfile.readlines()
+        for i in contents:
+            if i.find('Python')>=0:
+                print i
+        myfile.close()
+    except IOError as e:
+        print e
+
+def lab12():
+    W12main1()
+
+def main():
+    lab12()
+
+if __name__=="__main__": 
+     main()
